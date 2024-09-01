36fcd1f80e9c76470322ed8059c7d5500bfb9c48
Luke Yue <lukedyue@gmail.com>
2024-08-31 19:08:58 -0700

    feat(qp): implement qp_attr_mask with bitmask_enum
    Signed-off-by: Luke Yue <lukedyue@gmail.com>

--- examples/test_qp.rs
+++ examples/test_qp.rs
@@ -12,9 +12,9 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         let ctx = device.open().unwrap();
 
         let pd = ctx.alloc_pd().unwrap();
-        let mr = pd.reg_managed_mr(64).unwrap();
+        let _mr = pd.reg_managed_mr(64).unwrap();
 
-        let comp_channel = ctx.create_comp_channel().unwrap();
+        let _comp_channel = ctx.create_comp_channel().unwrap();
         let mut cq_builder = ctx.create_cq_builder();
         let sq = cq_builder.setup_cqe(128).build().unwrap();
         let rq = cq_builder.setup_cqe(128).build().unwrap();
