4d416bbead50470e7450375470b4338a32790bcb
Luke Yue <lukedyue@gmail.com>
2025-01-19 14:41:11 +0800

    feat: add more APIs for reading device attribute
    Signed-off-by: Luke Yue <lukedyue@gmail.com>

--- src/ibverbs/device_context.rs
+++ src/ibverbs/device_context.rs
@@ -5,10 +5,11 @@ use std::mem::MaybeUninit;
 use std::ptr::{self, NonNull};
 
 use rdma_mummy_sys::{
-    _ibv_query_gid_table, ibv_alloc_pd, ibv_close_device, ibv_context, ibv_device_attr_ex, ibv_get_device_name,
-    ibv_gid_entry, ibv_mtu, ibv_port_attr, ibv_query_device_ex, ibv_query_gid, ibv_query_gid_type, ibv_query_port,
-    IBV_GID_TYPE_IB, IBV_GID_TYPE_ROCE_V1, IBV_GID_TYPE_ROCE_V2, IBV_GID_TYPE_SYSFS_IB_ROCE_V1,
-    IBV_GID_TYPE_SYSFS_ROCE_V2, IBV_LINK_LAYER_ETHERNET, IBV_LINK_LAYER_INFINIBAND,
+    _ibv_query_gid_ex, _ibv_query_gid_table, ibv_alloc_pd, ibv_close_device, ibv_context, ibv_device_attr_ex,
+    ibv_get_device_name, ibv_gid_entry, ibv_mtu, ibv_port_attr, ibv_port_state, ibv_query_device_ex, ibv_query_gid,
+    ibv_query_gid_type, ibv_query_port, IBV_GID_TYPE_IB, IBV_GID_TYPE_ROCE_V1, IBV_GID_TYPE_ROCE_V2,
+    IBV_GID_TYPE_SYSFS_IB_ROCE_V1, IBV_GID_TYPE_SYSFS_ROCE_V2, IBV_LINK_LAYER_ETHERNET, IBV_LINK_LAYER_INFINIBAND,
+    IBV_LINK_LAYER_UNSPECIFIED,
 };
 
 use super::address::{Gid, GidEntry};
@@ -23,7 +24,7 @@ unsafe impl Send for DeviceContext {}
 unsafe impl Sync for DeviceContext {}
 
 #[repr(u32)]
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
 pub enum Mtu {
     Mtu256 = ibv_mtu::IBV_MTU_256,
     Mtu512 = ibv_mtu::IBV_MTU_512,
@@ -45,6 +46,178 @@ impl From<u32> for Mtu {
     }
 }
 
+#[repr(u8)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+pub enum PortWidth {
+    Width1X = 1,
+    Width2X = 2,
+    Width4X = 4,
+    Width8X = 8,
+    Width12X = 12,
+}
+
+impl From<u8> for PortWidth {
+    fn from(width: u8) -> Self {
+        match width {
+            1 => PortWidth::Width1X,
+            16 => PortWidth::Width2X,
+            2 => PortWidth::Width4X,
+            4 => PortWidth::Width8X,
+            8 => PortWidth::Width12X,
+            _ => panic!("Unknown port width value: {width}"),
+        }
+    }
+}
+
+#[repr(u32)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+pub enum PortSpeed {
+    SingleDataRate = 1,
+    DoubleDataRate = (1 << 1),
+    QuadrupleDataRate = (1 << 2),
+    FourteenDataRateTen = (1 << 3),
+    FourteenDataRate = (1 << 4),
+    EnhancedDataRate = (1 << 5),
+    HighDataRate = (1 << 6),
+    NextDataRate = (1 << 7),
+    ExtendedDataRate = (1 << 8),
+}
+
+impl From<u32> for PortSpeed {
+    fn from(speed: u32) -> Self {
+        match speed {
+            1 => PortSpeed::SingleDataRate,
+            2 => PortSpeed::DoubleDataRate,
+            4 => PortSpeed::QuadrupleDataRate,
+            8 => PortSpeed::FourteenDataRateTen,
+            16 => PortSpeed::FourteenDataRate,
+            32 => PortSpeed::EnhancedDataRate,
+            64 => PortSpeed::HighDataRate,
+            128 => PortSpeed::NextDataRate,
+            256 => PortSpeed::ExtendedDataRate,
+            _ => panic!("Unknown port speed value: {speed}"),
+        }
+    }
+}
+
+impl PortSpeed {
+    /// According to the [wikipedia](https://en.wikipedia.org/wiki/InfiniBand),
+    /// InfiniBand speed has signaling rate and actual rate (throughput), just as below
+    ///
+    /// | Generation | Release Year | Line Code | Signaling Rate (Gbit/s) | Throughput (Gbit/s) | Latency (μs) |
+    /// |------------|--------------|-----------|-------------------------|---------------------|--------------|
+    /// | SDR        | 2001/2003    | 8b/10b    | 2.5                     | 2.0                 | 5.0          |
+    /// | DDR        | 2005         | 8b/10b    | 5.0                     | 4.0                 | 2.5          |
+    /// | QDR        | 2007         | 8b/10b    | 10.0                    | 8.0                 | 1.3          |
+    /// | FDR10      | 2011         | 64b/66b   | 10.3125                 | 10.0                | 0.7          |
+    /// | FDR        | 2011         | 64b/66b   | 14.0625                 | 13.64               | 0.7          |
+    /// | EDR        | 2014         | 64b/66b   | 25.78125                | 25.0                | 0.5          |
+    /// | HDR        | 2018         | PAM4      | 53.125                  | 50.0                | <0.6         |
+    /// | NDR        | 2022         | 256b/257b | 106.25                  | 100.0               | N/A          |
+    /// | XDR        | 2024         | TBD       | 200.0                   | 200.0               | TBD          |
+    /// | GDR        | TBA          | TBD       | 400.0                   | 400.0               | TBD          |
+    pub fn to_signaling_rate(&self) -> f64 {
+        match self {
+            PortSpeed::SingleDataRate => 2.5,
+            PortSpeed::DoubleDataRate => 5.0,
+            PortSpeed::QuadrupleDataRate => 10.0,
+            PortSpeed::FourteenDataRateTen => 10.3125,
+            PortSpeed::FourteenDataRate => 14.0625,
+            PortSpeed::EnhancedDataRate => 25.78125,
+            PortSpeed::HighDataRate => 53.125,
+            PortSpeed::NextDataRate => 106.25,
+            PortSpeed::ExtendedDataRate => 250.0,
+        }
+    }
+
+    pub fn to_throughput(&self) -> f64 {
+        match self {
+            PortSpeed::SingleDataRate => 2.0,
+            PortSpeed::DoubleDataRate => 4.0,
+            PortSpeed::QuadrupleDataRate => 8.0,
+            PortSpeed::FourteenDataRateTen => 10.0,
+            PortSpeed::FourteenDataRate => 13.64,
+            PortSpeed::EnhancedDataRate => 25.0,
+            PortSpeed::HighDataRate => 50.0,
+            PortSpeed::NextDataRate => 100.0,
+            PortSpeed::ExtendedDataRate => 250.0,
+        }
+    }
+}
+
+#[repr(u8)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+pub enum LinkLayer {
+    Unspecified = IBV_LINK_LAYER_UNSPECIFIED,
+    Infiniband = IBV_LINK_LAYER_INFINIBAND,
+    Ethernet = IBV_LINK_LAYER_ETHERNET,
+}
+
+impl From<u8> for LinkLayer {
+    fn from(link: u8) -> Self {
+        match link {
+            IBV_LINK_LAYER_UNSPECIFIED => LinkLayer::Unspecified,
+            IBV_LINK_LAYER_INFINIBAND => LinkLayer::Infiniband,
+            IBV_LINK_LAYER_ETHERNET => LinkLayer::Ethernet,
+            _ => panic!("Unknown link layer value: {link}"),
+        }
+    }
+}
+
+#[repr(u32)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+pub enum PortState {
+    Nop = ibv_port_state::IBV_PORT_NOP,
+    Down = ibv_port_state::IBV_PORT_DOWN,
+    Initializing = ibv_port_state::IBV_PORT_INIT,
+    Armed = ibv_port_state::IBV_PORT_ARMED,
+    Active = ibv_port_state::IBV_PORT_ACTIVE,
+    ActiveDefer = ibv_port_state::IBV_PORT_ACTIVE_DEFER,
+}
+
+impl From<u32> for PortState {
+    fn from(port_state: u32) -> Self {
+        match port_state {
+            ibv_port_state::IBV_PORT_NOP => PortState::Nop,
+            ibv_port_state::IBV_PORT_DOWN => PortState::Down,
+            ibv_port_state::IBV_PORT_INIT => PortState::Initializing,
+            ibv_port_state::IBV_PORT_ARMED => PortState::Armed,
+            ibv_port_state::IBV_PORT_ACTIVE => PortState::Active,
+            ibv_port_state::IBV_PORT_ACTIVE_DEFER => PortState::ActiveDefer,
+            _ => panic!("Unknown port state value: {port_state}"),
+        }
+    }
+}
+
+#[repr(u8)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+pub enum PhysicalState {
+    NoStateChange,
+    Sleep,
+    Polling,
+    Disabled,
+    PortConfigurationTraining,
+    LinkUp,
+    LinkErrorRecovery,
+    PhyTest,
+}
+
+impl From<u8> for PhysicalState {
+    fn from(phy_state: u8) -> Self {
+        match phy_state {
+            0 => PhysicalState::NoStateChange,
+            1 => PhysicalState::Sleep,
+            2 => PhysicalState::Polling,
+            3 => PhysicalState::Disabled,
+            4 => PhysicalState::PortConfigurationTraining,
+            5 => PhysicalState::LinkUp,
+            6 => PhysicalState::LinkErrorRecovery,
+            7 => PhysicalState::PhyTest,
+            _ => panic!("Unknown port state value: {phy_state}"),
+        }
+    }
+}
+
 pub struct PortAttr {
     attr: ibv_port_attr,
 }
@@ -62,8 +235,27 @@ impl PortAttr {
         self.attr.gid_tbl_len
     }
 
-    pub fn link_layer(&self) -> u8 {
-        self.attr.link_layer
+    pub fn link_layer(&self) -> LinkLayer {
+        self.attr.link_layer.into()
+    }
+
+    pub fn port_state(&self) -> PortState {
+        self.attr.state.into()
+    }
+
+    pub fn phys_state(&self) -> PhysicalState {
+        self.attr.phys_state.into()
+    }
+
+    pub fn active_width(&self) -> PortWidth {
+        self.attr.active_width.into()
+    }
+
+    pub fn active_speed(&self) -> PortSpeed {
+        if self.attr.active_speed_ex != 0 {
+            return self.attr.active_speed_ex.into();
+        }
+        (self.attr.active_speed as u32).into()
     }
 }
 
@@ -78,6 +270,22 @@ impl DeviceAttr {
 
     pub fn completion_timestamp_mask(&self) -> u64 {
         self.attr.completion_timestamp_mask
+    }
+
+    pub fn vendor_id(&self) -> u32 {
+        self.attr.orig_attr.vendor_id
+    }
+
+    pub fn vendor_part_id(&self) -> u32 {
+        self.attr.orig_attr.vendor_part_id
+    }
+
+    pub fn firmware_version(&self) -> [i8; 64] {
+        self.attr.orig_attr.fw_ver
+    }
+
+    pub fn hardware_version(&self) -> u32 {
+        self.attr.orig_attr.hw_ver
     }
 }
 
@@ -146,6 +354,25 @@ impl DeviceContext {
         }
     }
 
+    pub fn query_gid_ex(&self, port_num: u8, gid_index: u32) -> Result<GidEntry, String> {
+        let mut entry = GidEntry::default();
+        unsafe {
+            match _ibv_query_gid_ex(
+                self.context,
+                port_num as u32,
+                gid_index,
+                &mut entry.0,
+                0,
+                size_of::<GidEntry>(),
+            ) {
+                0 => Ok(entry),
+                ret => Err(format!(
+                    "Failed to query gid_index {gid_index} on port {port_num}, returned {ret}"
+                )),
+            }
+        }
+    }
+
     pub fn query_gid_type(&self, port_num: u8, gid_index: u32) -> Result<u32, String> {
         let mut gid_type = u32::default();
         unsafe {
@@ -177,10 +404,10 @@ impl DeviceContext {
 
                     unsafe {
                         gid_type = match self.query_gid_type(port_num, gid_index as u32).unwrap_unchecked() {
-                            IBV_GID_TYPE_SYSFS_IB_ROCE_V1 if port_attr.link_layer() == IBV_LINK_LAYER_INFINIBAND => {
+                            IBV_GID_TYPE_SYSFS_IB_ROCE_V1 if port_attr.link_layer() == LinkLayer::Infiniband => {
                                 IBV_GID_TYPE_IB
                             },
-                            IBV_GID_TYPE_SYSFS_IB_ROCE_V1 if port_attr.link_layer() == IBV_LINK_LAYER_ETHERNET => {
+                            IBV_GID_TYPE_SYSFS_IB_ROCE_V1 if port_attr.link_layer() == LinkLayer::Ethernet => {
                                 IBV_GID_TYPE_ROCE_V1
                             },
                             IBV_GID_TYPE_SYSFS_ROCE_V2 => IBV_GID_TYPE_ROCE_V2,
@@ -268,7 +495,113 @@ impl DeviceContext {
 
 #[cfg(test)]
 mod tests {
+    use super::*;
     use crate::ibverbs::device;
+
+    #[test]
+    fn test_mtu_conversion() {
+        assert_eq!(Mtu::from(ibv_mtu::IBV_MTU_256), Mtu::Mtu256);
+        assert_eq!(Mtu::from(ibv_mtu::IBV_MTU_512), Mtu::Mtu512);
+        assert_eq!(Mtu::from(ibv_mtu::IBV_MTU_1024), Mtu::Mtu1024);
+        assert_eq!(Mtu::from(ibv_mtu::IBV_MTU_2048), Mtu::Mtu2048);
+        assert_eq!(Mtu::from(ibv_mtu::IBV_MTU_4096), Mtu::Mtu4096);
+    }
+
+    #[test]
+    #[should_panic(expected = "Unknown MTU value")]
+    fn test_invalid_mtu_conversion() {
+        let _ = Mtu::from(999);
+    }
+
+    #[test]
+    fn test_port_width_conversion() {
+        assert_eq!(PortWidth::from(1), PortWidth::Width1X);
+        assert_eq!(PortWidth::from(16), PortWidth::Width2X);
+        assert_eq!(PortWidth::from(2), PortWidth::Width4X);
+        assert_eq!(PortWidth::from(4), PortWidth::Width8X);
+        assert_eq!(PortWidth::from(8), PortWidth::Width12X);
+    }
+
+    #[test]
+    fn test_port_width_multiplier() {
+        assert_eq!(PortWidth::Width1X as u8, 1);
+        assert_eq!(PortWidth::Width2X as u8, 2);
+        assert_eq!(PortWidth::Width4X as u8, 4);
+        assert_eq!(PortWidth::Width8X as u8, 8);
+        assert_eq!(PortWidth::Width12X as u8, 12);
+    }
+
+    #[test]
+    fn test_port_speed_conversion() {
+        assert_eq!(PortSpeed::from(1), PortSpeed::SingleDataRate);
+        assert_eq!(PortSpeed::from(2), PortSpeed::DoubleDataRate);
+        assert_eq!(PortSpeed::from(4), PortSpeed::QuadrupleDataRate);
+        assert_eq!(PortSpeed::from(8), PortSpeed::FourteenDataRateTen);
+        assert_eq!(PortSpeed::from(16), PortSpeed::FourteenDataRate);
+        assert_eq!(PortSpeed::from(32), PortSpeed::EnhancedDataRate);
+        assert_eq!(PortSpeed::from(64), PortSpeed::HighDataRate);
+        assert_eq!(PortSpeed::from(128), PortSpeed::NextDataRate);
+        assert_eq!(PortSpeed::from(256), PortSpeed::ExtendedDataRate);
+    }
+
+    #[test]
+    fn test_port_speed_rates() {
+        assert_eq!(PortSpeed::SingleDataRate.to_signaling_rate(), 2.5);
+        assert_eq!(PortSpeed::SingleDataRate.to_throughput(), 2.0);
+
+        assert_eq!(PortSpeed::DoubleDataRate.to_signaling_rate(), 5.0);
+        assert_eq!(PortSpeed::DoubleDataRate.to_throughput(), 4.0);
+
+        assert_eq!(PortSpeed::QuadrupleDataRate.to_signaling_rate(), 10.0);
+        assert_eq!(PortSpeed::QuadrupleDataRate.to_throughput(), 8.0);
+
+        assert_eq!(PortSpeed::FourteenDataRateTen.to_signaling_rate(), 10.3125);
+        assert_eq!(PortSpeed::FourteenDataRateTen.to_throughput(), 10.0);
+
+        assert_eq!(PortSpeed::FourteenDataRate.to_signaling_rate(), 14.0625);
+        assert_eq!(PortSpeed::FourteenDataRate.to_throughput(), 13.64);
+
+        assert_eq!(PortSpeed::EnhancedDataRate.to_signaling_rate(), 25.78125);
+        assert_eq!(PortSpeed::EnhancedDataRate.to_throughput(), 25.0);
+
+        assert_eq!(PortSpeed::HighDataRate.to_signaling_rate(), 53.125);
+        assert_eq!(PortSpeed::HighDataRate.to_throughput(), 50.0);
+
+        assert_eq!(PortSpeed::NextDataRate.to_signaling_rate(), 106.25);
+        assert_eq!(PortSpeed::NextDataRate.to_throughput(), 100.0);
+
+        assert_eq!(PortSpeed::ExtendedDataRate.to_signaling_rate(), 250.0);
+        assert_eq!(PortSpeed::ExtendedDataRate.to_throughput(), 250.0);
+    }
+
+    #[test]
+    fn test_link_layer_conversion() {
+        assert_eq!(LinkLayer::from(IBV_LINK_LAYER_UNSPECIFIED), LinkLayer::Unspecified);
+        assert_eq!(LinkLayer::from(IBV_LINK_LAYER_INFINIBAND), LinkLayer::Infiniband);
+        assert_eq!(LinkLayer::from(IBV_LINK_LAYER_ETHERNET), LinkLayer::Ethernet);
+    }
+
+    #[test]
+    fn test_port_state_conversion() {
+        assert_eq!(PortState::from(0), PortState::Nop);
+        assert_eq!(PortState::from(1), PortState::Down);
+        assert_eq!(PortState::from(2), PortState::Initializing);
+        assert_eq!(PortState::from(3), PortState::Armed);
+        assert_eq!(PortState::from(4), PortState::Active);
+        assert_eq!(PortState::from(5), PortState::ActiveDefer);
+    }
+
+    #[test]
+    fn test_physical_state_conversion() {
+        assert_eq!(PhysicalState::from(0), PhysicalState::NoStateChange);
+        assert_eq!(PhysicalState::from(1), PhysicalState::Sleep);
+        assert_eq!(PhysicalState::from(2), PhysicalState::Polling);
+        assert_eq!(PhysicalState::from(3), PhysicalState::Disabled);
+        assert_eq!(PhysicalState::from(4), PhysicalState::PortConfigurationTraining);
+        assert_eq!(PhysicalState::from(5), PhysicalState::LinkUp);
+        assert_eq!(PhysicalState::from(6), PhysicalState::LinkErrorRecovery);
+        assert_eq!(PhysicalState::from(7), PhysicalState::PhyTest);
+    }
 
     #[test]
     fn test_query_gid_table_fallback() -> Result<(), Box<dyn std::error::Error>> {
