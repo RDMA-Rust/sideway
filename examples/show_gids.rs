3d3c45bd0691bfb3f280c8ab2c249453c601c1d8
Luke Yue <lukedyue@gmail.com>
2025-02-24 23:30:52 +0800

    refactor(ibverbs): return String instead of Option<String> for device name
    RDMA device would have a device name 99% of time, when it really doesn't
    have one because of buggy configuration, we could still use an empty
    string for it.
    
    Signed-off-by: Luke Yue <lukedyue@gmail.com>

--- examples/show_gids.rs
+++ examples/show_gids.rs
@@ -42,26 +42,24 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     for device in &device_list {
         let ctx = device.open().unwrap();
 
-        if let Some(name) = device.name() {
-            let gid_entries = ctx.query_gid_table().unwrap();
+        let name = device.name();
 
-            for gid in gid_entries {
-                devices.push(GidEntries {
-                    dev_name: name.clone(),
-                    port: gid.port_num(),
-                    index: gid.gid_index() as _,
-                    gid: gid.gid(),
-                    ipv4: Ipv6Addr::from(gid.gid()).to_ipv4_mapped(),
-                    ver: match gid.gid_type() {
-                        GidType::InfiniBand => "IB",
-                        GidType::RoceV1 => "RoCEv1",
-                        GidType::RoceV2 => "RoCEv2",
-                    },
-                    netdev: gid.netdev_name().unwrap(),
-                });
-            }
-        } else {
-            eprintln!("Found a device without a name, skipping.");
+        let gid_entries = ctx.query_gid_table().unwrap();
+
+        for gid in gid_entries {
+            devices.push(GidEntries {
+                dev_name: name.clone(),
+                port: gid.port_num(),
+                index: gid.gid_index() as _,
+                gid: gid.gid(),
+                ipv4: Ipv6Addr::from(gid.gid()).to_ipv4_mapped(),
+                ver: match gid.gid_type() {
+                    GidType::InfiniBand => "IB",
+                    GidType::RoceV1 => "RoCEv1",
+                    GidType::RoceV2 => "RoCEv2",
+                },
+                netdev: gid.netdev_name().unwrap(),
+            });
         }
     }
 
